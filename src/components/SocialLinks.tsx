e775c1eecef3a7a316fefb6dec488f0e26b3ce2f
John Doty <johnrdoty92@gmail.com>
2026-01-04 08:34:39 -0500

    chore: move social link models

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -9,14 +9,14 @@ export const SocialLinks = () => {
 
   return (
     <Suspense fallback={<></>}>
-      <SocialMediaMinifigure minifigure="Overalls" position={[6.5, 0, 5.5]} />
-      <SocialMediaMinifigure minifigure="Knight" position={[8, 0, 5]} />
       <SocialMediaMinifigure
-        minifigure="Spaceman"
-        position={[5.5, 0, 6.5]}
-        rotation-y={Math.PI / 2}
+        minifigure="Overalls"
+        position={[-5.5, 0, 6.5]}
+        rotation-y={-Math.PI / 2}
       />
-      <SocialMediaMinifigure minifigure="Pirate" position={[5, 0, 8]} rotation-y={Math.PI / 2} />
+      <SocialMediaMinifigure minifigure="Knight" position={[-5, 0, 8]} rotation-y={-Math.PI / 2} />
+      <SocialMediaMinifigure minifigure="Spaceman" position={[-6.5, 0, 5.5]} rotation-y={0} />
+      <SocialMediaMinifigure minifigure="Pirate" position={[-8, 0, 5]} rotation-y={0} />
     </Suspense>
   );
 };
