87ef9bda495fb775fd1f4688581de608f221ebe0
John Doty <johnrdoty92@gmail.com>
2026-01-19 14:45:28 -0500

    chore: remove draco

--- src/hooks/useGLTF.ts
+++ src/hooks/useGLTF.ts
@@ -1,15 +1,9 @@
 import { useLoader, type ObjectMap } from "@react-three/fiber";
 import { MeshoptDecoder } from "three/examples/jsm/libs/meshopt_decoder.module.js";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import { GLTFLoader, type GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
-
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath("/draco");
-dracoLoader.preload();
 
 const extensions = (loader: GLTFLoader) => {
   loader.setMeshoptDecoder(MeshoptDecoder);
-  loader.setDRACOLoader(dracoLoader);
 };
 
 const useGLTF = <T extends Partial<ObjectMap> = Partial<ObjectMap>>(path: string): GLTF & T =>
