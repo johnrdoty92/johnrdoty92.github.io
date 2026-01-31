81d55699dea6e1724e6b5a2b639524c40f586c83
John Doty <johnrdoty92@gmail.com>
2026-01-31 16:47:52 -0500

    refactor: lerp skills bricks animation

--- src/components/Brick.tsx
+++ src/components/Brick.tsx
@@ -6,30 +6,19 @@ import {
   drawCanvasTexture,
   type CanvasDrawingOptions,
 } from "../util/canvasTexture";
-import {
-  Color,
-  MathUtils,
-  Mesh,
-  MeshPhysicalMaterial,
-  SRGBColorSpace,
-  type Vector3Tuple,
-} from "three";
+import { Color, MeshPhysicalMaterial, SRGBColorSpace } from "three";
 import { useToggleAnimationState } from "../hooks/useToggleAnimationState";
 
 export type BrickProps = {
   visibility: "normal" | "dimmed" | "selected";
-  position: Vector3Tuple;
-  delay: number;
 } & CanvasDrawingOptions &
   Omit<ThreeElements["mesh"], "position">;
 
 const visible = new Color("white");
+// TODO: use theme color
 const dimmed = new Color(0.04, 0.05, 0.2);
-const startingRotation = Math.PI / 3;
-const targetRotation = 0;
-const startingHeight = 8;
 
-export const Brick = ({ label, icon, color, visibility, delay, ...props }: BrickProps) => {
+export const Brick = ({ label, icon, color, visibility, ...props }: BrickProps) => {
   const canvas = useMemo(() => {
     const node = document.createElement("canvas");
     node.width = canvasTextureSize;
@@ -39,36 +28,14 @@ export const Brick = ({ label, icon, color, visibility, delay, ...props }: Brick
     return node;
   }, [label, icon, color]);
 
-  const brick = useRef<Mesh>(null!);
   const material = useRef<MeshPhysicalMaterial>(null!);
 
   useToggleAnimationState(visibility === "dimmed", (alpha) => {
     material.current.color.lerpColors(visible, dimmed, alpha);
   });
 
-  useToggleAnimationState(
-    true,
-    (alpha) => {
-      const targetY = props.position[1];
-      const currentY = MathUtils.lerp(targetY + startingHeight, targetY, alpha);
-      brick.current.position.y = currentY;
-      brick.current.rotation.z = MathUtils.lerp(
-        startingRotation,
-        targetRotation,
-        MathUtils.smoothstep(alpha, 0.75, 1),
-      );
-    },
-    { delay: 250 * delay },
-  );
-
   return (
-    <mesh
-      {...props}
-      position={[props.position[0], props.position[1] + startingHeight, props.position[2]]}
-      geometry={brickGeometry}
-      ref={brick}
-      rotation-z={startingRotation}
-    >
+    <mesh {...props} geometry={brickGeometry}>
       <meshPhysicalMaterial
         ref={material}
         ior={1.4}
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,10 +5,19 @@ import { useSearchValue } from "../hooks/useSearchValue";
 import { useMediaQuery } from "../hooks/useMediaQuery";
 import { MOBILE_BREAKPOINT_QUERY } from "../constants/styles";
 import { SKILLS } from "../constants/skills";
+import { MathUtils, type Group } from "three";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const fuse = new Fuse(SKILLS, { keys: ["name", "tags"], threshold: 0.25 });
 
+const STARTING_ROTATION = Math.PI / 3;
+const TARGET_ROTATION = 0;
+const STARTING_HEIGHT = 8;
+const SPACING = 2;
+
 export const Skills = () => {
+  const bricks = useRef<Group>(null!);
   const searchValue = useSearchValue();
   const results = fuse.search(searchValue);
   const matches = new Set(results.map(({ item }) => item.name));
@@ -17,34 +26,60 @@ export const Skills = () => {
   const columnCount = isMobileScreen ? 2 : 3;
   const columnHeight = Math.floor(SKILLS.length / columnCount);
   const wallOffset = isMobileScreen ? 2 : 3;
-  const spacing = 2;
-  const maxDistance = (columnCount - 1) * spacing + wallOffset;
+  const maxDistance = (columnCount - 1) * SPACING + wallOffset;
   const maxHeight = (columnHeight - 1) * brickHeight;
+
+  const targets = SKILLS.map((_, i) => {
+    const column = Math.floor(i / columnHeight);
+    const isLastColumn = column === columnCount - 1;
+    const isOddColumn = column % 2 === 1;
+    const rotation = isLastColumn || isOddColumn ? Math.PI / 2 : 0;
+    const x = SPACING * column + wallOffset;
+    const y = maxHeight - (i % columnHeight) * brickHeight;
+    const z = maxDistance - x + wallOffset;
+    return { x, y, z, rotation, column };
+  });
 
   // TODO: sort skills so they appear from top to bottom in order of importance
   // TODO: handle placement if there's overflow. Currently starts by floating at the top
 
-  return SKILLS.map(({ name, color, path }, i) => {
-    const column = Math.floor(i / columnHeight);
-    const height = maxHeight - (i % columnHeight) * brickHeight;
-    const isLastColumn = column === columnCount - 1;
-    const isOddColumn = column % 2 === 1;
-    const rotationY = isLastColumn || isOddColumn ? Math.PI / 2 : 0;
-    const isMatch = matches.has(name) || searchValue === "";
-    const x = spacing * column + wallOffset;
-    const z = maxDistance - x + wallOffset;
+  const progress = useRef(0);
+  const overlap = 0.1;
+  const duration = 2.5;
 
-    return (
-      <Brick
-        key={i}
-        delay={height + x}
-        label={name}
-        color={color}
-        icon={path}
-        visibility={isMatch ? "normal" : "dimmed"}
-        rotation-y={rotationY}
-        position={[x, height, z]}
-      />
-    );
+  useFrame((_, delta) => {
+    progress.current = MathUtils.clamp(progress.current + delta, 0, duration);
+    const totalAlpha = progress.current / duration;
+    const chunk = 1 / bricks.current.children.length;
+    bricks.current.children.forEach((brick, i) => {
+      const index = i;
+      const { x, y, z, rotation, column } = targets[index];
+      const columnTotal = (column + 1) * columnHeight - 1;
+      const animationIndex = (columnTotal - i) * columnCount + column;
+      const start = chunk * animationIndex - chunk * animationIndex * overlap;
+      const stop = chunk * (animationIndex + 1) + (1 - chunk * (animationIndex + 1)) * overlap;
+      const alpha = MathUtils.smoothstep(totalAlpha, start, stop);
+      brick.position.set(x, MathUtils.lerp(y + STARTING_HEIGHT, y, alpha), z);
+      brick.rotation.y = rotation;
+      const rotationAlpha = MathUtils.smoothstep(alpha, 0.8, 1);
+      brick.rotation.z = MathUtils.lerp(STARTING_ROTATION, TARGET_ROTATION, rotationAlpha);
+    });
   });
+
+  return (
+    <group ref={bricks}>
+      {SKILLS.map(({ name, color, path }, i) => {
+        const isMatch = matches.has(name) || searchValue === "";
+        return (
+          <Brick
+            key={i}
+            visibility={isMatch ? "normal" : "dimmed"}
+            label={name}
+            color={color}
+            icon={path}
+          />
+        );
+      })}
+    </group>
+  );
 };
