ca9526eeaac013b3eb00adc53e378cd19397f986
John Doty <johnrdoty92@gmail.com>
2026-02-07 13:51:13 -0500

    chore: clean up lighting component

--- src/components/Lighting.tsx
+++ src/components/Lighting.tsx
@@ -1,7 +1,1 @@
-export const Lighting = () => {
-  return (
-    <>
-      <directionalLight position={[0, 1, 0]} args={["white", 1]} />
-    </>
-  );
-};
+export const Lighting = () => <directionalLight position={[0, 1, 0]} args={["white", 1]} />;
