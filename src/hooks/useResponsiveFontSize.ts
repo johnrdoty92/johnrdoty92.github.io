b6c431be9a16a9783ce743135ba593a6008ec9a9
John Doty <johnrdoty92@gmail.com>
2026-02-28 08:19:48 -0500

    fix: clamp font size

--- src/hooks/useResponsiveFontSize.ts
+++ src/hooks/useResponsiveFontSize.ts
@@ -5,5 +5,5 @@ const MIN = 0.25;
 const MAX = 1;
 export const useResponsiveFontSize = () => {
   const aspect = useThree(({ size }) => size.width / size.height);
-  return MathUtils.mapLinear(aspect, 0, 1, MIN, MAX);
+  return MathUtils.clamp(MathUtils.mapLinear(aspect, 0, 1, MIN, MAX), MIN, MAX);
 };
