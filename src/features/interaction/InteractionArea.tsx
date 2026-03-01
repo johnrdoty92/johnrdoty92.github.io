3911f2b6fce2d8dece79f5a78fb80d3803d0a833
John Doty <johnrdoty92@gmail.com>
2026-03-01 17:29:48 -0500

    chore: remove swipe prompt

--- src/features/interaction/InteractionArea.tsx
+++ src/features/interaction/InteractionArea.tsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from "react";
 import { Search } from "./Search";
-import { SwipePrompt } from "./SwipePrompt";
 import { CarouselButtons } from "./CarouselButtons";
 
 const DownloadResumeButton = lazy(() => import("@/features/interaction/DownloadResumeButton"));
@@ -9,7 +8,6 @@ export const InteractionArea = () => {
   return (
     <div className="interaction-area">
       <Search />
-      <SwipePrompt />
       <div className="actions">
         <CarouselButtons>
           <Suspense
--- src/features/interaction/index.ts
+++ src/features/interaction/index.ts
@@ -2,4 +2,3 @@ export * from "./CarouselButtons";
 export * from "./InteractionArea";
 export * from "./Modal";
 export * from "./Search";
-export * from "./SwipePrompt";
